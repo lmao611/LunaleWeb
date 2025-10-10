66a035a601ee188d11a3e6c379f39b3321b4ff65
Dai <vandai06112007@gmail.com>
2025-10-10 17:55:31 +0700

    lmoa

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors({
@@ -24,6 +26,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/collections", collectionRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*",(req,res) => {
+    res.sendFile(path.resolve(__dirname, "frontend","dist","index.html"))
+  })
+}
 
 app.listen(PORT, () => {
   console.log("🚀 Server is running on http://localhost:" + PORT);
