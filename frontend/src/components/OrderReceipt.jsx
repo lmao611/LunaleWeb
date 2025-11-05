1b133ff354905ce21956449d6c01726c464183d8
Dai <vandai06112007@gmail.com>
2025-11-05 13:31:56 +0700

    lkl

--- frontend/src/components/OrderReceipt.jsx
+++ frontend/src/components/OrderReceipt.jsx
@@ -373,10 +373,17 @@ const OrderReceipt = () => {
 
       {/* ===== HIDDEN PRINT VIEW ===== */}
       <div
-        ref={printRef}
-        style={{ display: "none" }}
-        className="w-[1000px] bg-white text-gray-900 font-sans p-8 border-2 border-gray-200 rounded-xl"
-      >
+  ref={printRef}
+  style={{
+    position: "absolute",
+    top: "-9999px",
+    left: "-9999px",
+    opacity: 0,
+    pointerEvents: "none",
+  }}
+  className="w-[1000px] bg-white text-gray-900 font-sans p-8 border-2 border-gray-200 rounded-xl"
+>
+
         <div className="flex justify-between items-center mb-6">
           <img src="/lunale.png" alt="Logo" className="h-12" />
           <h2 className="text-2xl font-bold text-center text-blue-700 flex-1">
