b6e5b8f9c20dccff929464bff911e8325a56f92d
Dai <vandai06112007@gmail.com>
2025-12-12 15:28:42 +0700

    lp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsForceLoadingDone(true);
-    }, 1000);
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, []);
