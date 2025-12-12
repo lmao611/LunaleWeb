65f27b971e5ef48744ee5c8ec0f7908fd7d3d7ff
Dai <vandai06112007@gmail.com>
2025-12-12 10:12:03 +0700

    lp

--- backend/controllers/auth.cotroller.js
+++ backend/controllers/auth.cotroller.js
@@ -135,7 +135,7 @@ export const login = async (req, res) => {
                 },
             });
         } else {
-            res.status(401).json({ message: "Invalid email or password" });
+            res.status(401).json({ message: "Mật khẩu hoặc tên tài khoản không đúng" });
         }
     } catch (error) {
         console.log("Error in login controller:", error.message);
