f07474f6570a4b2bbcf362a00669f3524770828a
Dai <vandai06112007@gmail.com>
2026-03-21 19:30:16 +0700

    pol

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -206,7 +206,7 @@ const LoginPage = () => {
             Chưa có tài khoản?{" "}
             <Link
               to="/signup"
-              className="font-medium text-shadow-cyan-600 hover:text-shadow-sky-400 inline-flex items-center"
+              className="font-medium text-shadow-blue-600 hover:text-cyan-500 inline-flex items-center"
             >
               Đăng ký ngay
               <ArrowRight className="ml-1 h-4 w-4" />
